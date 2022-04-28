48088d4efc8a5aff16f79fcc53283bf0d7932821
farmology <abkim03@gmail.com>
2022-04-27 23:59:51 -0700

    Add fetch function and type display
    Add a fetch function to search by pokemon name and return its type(s) to
    the type display

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
-const searchbtn = document.getElementById('searchbtn');
+const searchbutton = document.getElementById('searchbtn');
+const poketype = document.getElementById('poketype');
 
-searchbtn.addEventListener('click', getFetch);
+searchbutton.addEventListener('click', function(event) {
+    getFetch();
+    event.preventDefault();
+});
 
+// function getFetch() {
+//     let pokemon = document.getElementById('pokename').value;
+//     console.log(pokemon);
+//     let url = `https://pokeapi.co/api/v2/pokemon/${pokemon}/`;
 
-function getFetch() {
-    let pokemon = document.getElementById('pokename').value
-    let url = `https://pokeapi.co/api/v2/pokemon/${pokemon}/`
+//     fetch(url)
+//         .then(res => res.json())
+//         .then(data => {
+//             console.log(data)
+//         .catch(err => {
+//             console.log('error ${err}')
+//         });   
+        
+//     }
 
-    fetch(url)
-    .then(data => {
-        console.log(data)
-    })
+async function getFetch() {
+    let pokemon = document.getElementById('pokename').value;
+    let res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}/`);
+    let json = await res.json();
+    console.log(json.types);
+    json.types.forEach(element => {
+        console.log(element.type.name);
+        poketype.innerText += element.type.name;
+    });
 }
