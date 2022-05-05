eb297f2214226dfae70cde1226f1fd6663f56670
farmology <abkim03@gmail.com>
2022-05-05 01:20:07 -0700

    Add defending display for dual type pokemon
    Add display for quadruple, double, half, and quarter effectiveness for
    dual type pokemon.

--- script.js
+++ script.js
@@ -23,45 +23,122 @@ searchbutton.addEventListener("click", function (event) {
 //         });
 
 //     }
-async function createTypeList() {
-  let double = document.createElement("div");
-  double.id = "double";
-  let half = document.createElement("div");
-  half.id = "half";
-  let none = document.createElement("div");
-  none.id = "none";
-  attacking.appendChild(double);
-  attacking.appendChild(half);
-  attacking.appendChild(none);
+function createTypeList() {
+  let adouble = document.createElement("div");
+  adouble.id = "adouble";
+  let ahalf = document.createElement("div");
+  ahalf.id = "ahalf";
+  let anone = document.createElement("div");
+  anone.id = "anone";
+  let dquadruple = document.createElement("div");
+  dquadruple.id = 'dquadruple';
+  let ddouble = document.createElement("div");
+  ddouble.id = "ddouble";
+  let dhalf = document.createElement("div");
+  dhalf.id = "dhalf";
+  let dquarter = document.createElement("div");
+  dquarter.id = "dquarter";
+  let dnone = document.createElement("div");
+  dnone.id = "dnone";
+  attacking.appendChild(adouble);
+  attacking.appendChild(ahalf);
+  attacking.appendChild(anone);
+  defending.appendChild(dquadruple);
+  defending.appendChild(ddouble);
+  defending.appendChild(dhalf);
+  defending.appendChild(dquarter);
+  defending.appendChild(dnone);
 }
 
 async function getFetch() {
   let pokemon = document.getElementById("pokename").value.toLowerCase();
   let res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}/`);
   let json = await res.json();
-  let typeurl = await fetch(json.types[0].type.url);
-  let typejson = await typeurl.json();
-  console.log(typejson.damage_relations);
-  console.log(json.types);
+  let typeurl1 = await fetch(json.types[0].type.url);
+
+  let typejson1 = await typeurl1.json();
 
   poketype.innerText = "";
   if (json.types.length === 1) {
     poketype.innerText = json.types[0].type.name;
     createTypeList();
-    let doublestring = typejson.damage_relations.double_damage_to
+    let adoublestring = typejson1.damage_relations.double_damage_to
       .map((type) => type.name)
       .join(" ");
-    double.innerText = `Double Damage \n ${doublestring}`;
-    let halfstring = typejson.damage_relations.half_damage_to
+    adouble.innerText = `Double Damage to \n ${adoublestring}`;
+    let ahalfstring = typejson1.damage_relations.half_damage_to
       .map((type) => type.name)
       .join(" ");
-    half.innerText = `Half Damage \n ${halfstring}`;
-
+    ahalf.innerText = `Half Damage to \n ${ahalfstring}`;
+    let anonestring = typejson1.damage_relations.no_damage_to
+      .map((type) => type.name)
+      .join(" ");
+    anone.innerText = `No Damage to \n ${anonestring}`;
+    let ddoublestring = typejson1.damage_relations.double_damage_from
+      .map((type) => type.name)
+      .join(" ");
+    ddouble.innerText = `Double Damage from \n ${ddoublestring}`;
+    let dhalfstring = typejson1.damage_relations.half_damage_from
+      .map((type) => type.name)
+      .join(" ");
+    dhalf.innerText = `Half Damage from \n ${dhalfstring}`;
+    let dnonestring = typejson1.damage_relations.no_damage_from
+      .map((type) => type.name)
+      .join(" ");
+    dnone.innerText = `No Damage from \n ${dnonestring}`;
   } else {
+    let typeurl2 = await fetch(json.types[1].type.url);
+    let typejson2 = await typeurl2.json();
     poketype.innerText = `${json.types[0].type.name} & ${json.types[1].type.name}`;
-    let typeurl = await fetch(json.types[0].type.url);
-    let typejson = await typeurl.json();
-    console.log(typejson.damage_relations);
+    console.log(typejson1.damage_relations);
+    console.log(typejson2.damage_relations);
+    attacking.innerText = "";
+    defending.innerText = "";
+    createTypeList();
+    const darray1 = typejson1.damage_relations.double_damage_from.map(
+      (type) => type.name
+    );
+    const darray2 = typejson2.damage_relations.double_damage_from.map(
+      (type) => type.name
+    );
+    const fourxarray = darray1.filter((type) => darray2.includes(type));
+    let fourxstring = fourxarray.join(" ");
+    dquadruple.innerText = `Quadruple Damage from \n ${fourxstring}`;
+    const harray1 = typejson1.damage_relations.half_damage_from.map(
+      (type) => type.name
+    );
+    const harray2 = typejson2.damage_relations.half_damage_from.map(
+      (type) => type.name
+    );
+    const narray1 = typejson1.damage_relations.no_damage_from.map(
+      (type) => type.name
+    );
+    const narray2 = typejson2.damage_relations.no_damage_from.map(
+      (type) => type.name
+    );
+    const quarterxarray = harray1.filter((type) => harray2.includes(type));
+    let quarterxstring = quarterxarray.join(" ");
+    dquarter.innerText = `Quarter Damage from \n ${quarterxstring}`;
+    const halfxarray1 = harray1
+      .filter((type) => !harray2.includes(type))
+      .concat(harray2.filter((type) => !harray1.includes(type)));
+    const doublexarray1 = darray1
+      .filter((type) => !darray2.includes(type))
+      .concat(darray2.filter((type) => !darray1.includes(type)));
+    console.log(halfxarray1);
+    console.log(doublexarray1);
+    const halfxarray2 = halfxarray1
+        .filter((type) => !doublexarray1.includes(type));
+    const doublexarray2 = doublexarray1
+        .filter((type) => !halfxarray1.includes(type));
+    
+            
+    console.log(halfxarray2);
+    let halfxstring = halfxarray2.join(' ');
+    dhalf.innerText = `Half Damage from \n ${halfxstring}`;
+    let doublexstring = doublexarray2.join(' ');
+    ddouble.innerText = `Double Damage from \n ${doublexstring}`;
+
   }
 
   console.log(json.sprites.front_default);
